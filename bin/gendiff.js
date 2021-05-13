f91aaad9d940263b6971176d77539dfdac3df2df
Marina Merzlyakova <mrn.mrzlk@gmail.com>
2021-05-13 13:10:06 +0400

    step 2: add arguments and options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,8 @@ import { Command } from '../node_modules/commander/esm.mjs';
 const program = new Command();
 program
   .version('1.0.0', '-V, --version')
+  .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type>',  'output format')
 
 console.log(program.parse())
