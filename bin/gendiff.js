2845e1d44d41f1ea849e1850e3469a100da6d593
Marina Merzlyakova <mrn.mrzlk@gmail.com>
2021-05-18 16:01:00 +0400

    step 3: JSON compare

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 import program from 'commander';
+import genDiff from '../src/index.js';
 
 program
   .version('1.0.0', '-V, --version')
   .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format');
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
 
 console.log(program.parse());
