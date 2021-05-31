3c2c1bd567b886b2e42d0d0ff5ff0597025c212f
Marina Merzlyakova <mrn.mrzlk@gmail.com>
2021-05-31 16:00:42 +0400

    step 6: maintability was improved

--- src/diff.js
+++ src/diff.js
@@ -1,9 +1,7 @@
 import _ from 'lodash';
 
 const buildDiff = (data1, data2) => {
-  const keys1 = Object.keys(data1);
-  const keys2 = Object.keys(data2);
-  const keys = _.sortBy(_.union(keys1, keys2));
+  const keys = _.sortBy(_.union(_.keys(data1), _.keys(data2)));
 
   const difference = keys.map((key) => {
     const value1 = data1[key];
